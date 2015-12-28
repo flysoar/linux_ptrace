2b2b03536c46a93f60a415c61a114f9cfca5d407
flysoar <dark772390855@gmail.com>
2015-12-29 00:19:05 +0800

    start

---
+++ get_free_space_addr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+void* get_free_space_addr(pid_t pid)
+{
+    char filename[85];
+    char line[200];
+    char temp[200];
+    FILE *fp;
+    void *addr;
+    int flag=0;
+    sprintf(filename,"/proc/%d/maps",pid);
+    fp=fopen(filename,"r");
+    if(fp==NULL)
+        return NULL;
+    while(fgets(line,200,fp)!=NULL)
+    {
+        sscanf(line,"%lx-%*lx %*s %*s %s",&addr,temp);
+        if(!strncmp(temp,"00:00",strlen("00:00")))
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag)
+        return addr;
+    else
+        return NULL;
+}
+
---
+++ ptrace_get_put_data.c
@@ -0,0 +1,57 @@
+#include <sys/ptrace.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int ptrace_get_data(pid_t pid,void *local_addr,void *remote_addr,long len)
+{
+    union u{
+        long num;
+        char bytes[sizeof(long)];
+    }data;
+    char *addr=(char *)local_addr;
+    int max=len/sizeof(long);
+    int i;
+    for(i=0;i<max;i++)
+    {
+        data.num=ptrace(PTRACE_PEEKTEXT,pid,remote_addr+i*sizeof(long),NULL);
+        addr+=i*sizeof(long);
+        memcpy(addr,data.bytes,sizeof(long));
+    }
+    if(len%sizeof(long))
+    {
+        data.num=ptrace(PTRACE_PEEKTEXT,pid,remote_addr+i*sizeof(long),NULL);
+        addr+=i*sizeof(long);
+        memcpy(addr,data.bytes,len%sizeof(long));
+    }
+    return len;
+}
+
+int ptrace_put_data(pid_t pid,void *local_addr,void *remote_addr,long len)
+{
+    union u{
+        long num;
+        char bytes[sizeof(long)];
+    }data;
+    char *addr=(char *)local_addr;
+    int max=len/sizeof(long);
+    int i;
+    for(i=0;i<max;i++)
+    {
+        addr+=i*sizeof(long);
+        memcpy(data.bytes,addr,sizeof(long));
+        ptrace(PTRACE_POKETEXT,pid,remote_addr+i*sizeof(long),&data.num);
+    }
+    if(len%sizeof(long))
+    {
+        addr+=i*sizeof(long);
+        memcpy(data.bytes,addr,len%sizeof(long));
+        ptrace(PTRACE_POKETEXT,pid,remote_addr+i*sizeof(long),&data.num);
+    }
+    return len;
+}
+
+int main()
+{
+    return 0;
+}
